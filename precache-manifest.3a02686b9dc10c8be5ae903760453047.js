8854d91f857efa000364d8da147159b86279da40
AlexShuk <alexshokarev@gmail.com>
2020-08-08 16:36:59 +0300

    deploy

---
+++ precache-manifest.3a02686b9dc10c8be5ae903760453047.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d0f030f5f3c491602d",
+    "url": "/SPA-VUE-ABZ/css/app.1218b5f9.css"
+  },
+  {
+    "revision": "fed073996c0c4c7835f8d6a862ca21ea",
+    "url": "/SPA-VUE-ABZ/debug.log"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/SPA-VUE-ABZ/img/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/SPA-VUE-ABZ/img/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/SPA-VUE-ABZ/img/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "649734d81ef1f67f1712ee5269920711",
+    "url": "/SPA-VUE-ABZ/index.html"
+  },
+  {
+    "revision": "28d0f030f5f3c491602d",
+    "url": "/SPA-VUE-ABZ/js/app.ca082dd6.js"
+  },
+  {
+    "revision": "b25ae6632db7ecefb924",
+    "url": "/SPA-VUE-ABZ/js/chunk-vendors.215f41b1.js"
+  },
+  {
+    "revision": "153204c17305a8898a6ea954d2da6b62",
+    "url": "/SPA-VUE-ABZ/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/SPA-VUE-ABZ/robots.txt"
+  }
+]);
